3c2cd14e37faf07d2e677926ef2fee13829f4047
suvasish114 <misuvasish114@gmail.com>
2020-10-16 20:09:29 +0530

    insertion_sort() function updated

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
-void insertion_sort(int a[], int _a);
-/*MAIN FUNCTION---*/
+
+//used functions
+void insertion_sort(int *a, int n);
+
+//main method
 int main(){
     int N;
     std::cin>>N;
     int A[N];
-    //int *A=(int *)malloc(sizeof(int)*N);
     for(int i=1; i<=N; i++){
         std::cin>>A[i];
     }
@@ -14,9 +16,10 @@ int main(){
         std::cout<<A[i]<<" ";
     }
 }
-/*INSERTION SORT---*/
-void insertion_sort(int a[], int _a){
-    for(int i=2; i<=_a; i++){
+
+//insertion sort
+void insertion_sort(int *a, int n){
+    for(int i=2; i<=n; i++){
         int j=i-1;
         a[0]=a[i];
         while(a[0]<a[j]){
